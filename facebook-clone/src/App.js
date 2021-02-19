e7c21579f1efdbad273b9cfc530c1d0e9a6a7ff6
LektatiMahdi <lektati1@gmail.com>
2021-02-19 09:42:00 +0100

    initiale setup for react project

---
+++ facebook-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* header */}
+
+      {/* App body */}
+    
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* widgets */}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ facebook-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDNTXd7-IYFjnMUvhViwXowItpe15cecXA",
+  authDomain: "facebook-clone-a42fd.firebaseapp.com",
+  projectId: "facebook-clone-a42fd",
+  storageBucket: "facebook-clone-a42fd.appspot.com",
+  messagingSenderId: "5860423798",
+  appId: "1:5860423798:web:32f582f3c149ce3a748757",
+  measurementId: "G-YYNXVTZXSM"
+};
